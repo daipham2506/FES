cc6747f739508b2a626a0a91f99c5ef1954832a5
daipham2506 <daipham2506@gmail.com>
2020-05-31 21:52:02 +0700

    turn off dev-tool

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, "../build/index.html")}`)
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, "./build/index.html")}`)
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
